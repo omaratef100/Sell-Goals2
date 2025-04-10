ee3dde7ba1233c96fc1bfdd13ea3f29f3e767951
omaratef100 <omerabomosa306@gmail.com>
2025-04-10 18:12:22 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -19,3 +19,12 @@ document.addEventListener('DOMContentLoaded', () => {
 const navigate = (page) => window.location.href = `${page}.html`;
 const goToSignup = () => navigate('signup');
 const goToLogin = () => navigate('login');
+
+window.addEventListener('scroll', () => {
+    const header = document.getElementById('mainHeader');
+    if (window.scrollY > 50) {
+        header.classList.add('header-scrolled');
+    } else {
+        header.classList.remove('header-scrolled');
+    }
+});
